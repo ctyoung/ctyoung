774ac5c314457cb0c6fa93f17f92e4df4780f25a
ctyoung <chris@ctyoung.co.uk>
2024-05-29 15:38:21 +0100

    Delete Developer directory

--- Developer/config.py
+++
@@ -1,8 +0,0 @@
-# config for endlessMarvels
-# should be replaced with user's own details
-
-PUBLIC_KEY = "your Marvel public key here"
-PRIVATE_KEY = "your Marvel private key here"
-
-DATASIFT_USERNAME = "your Datasift username here"
-DATASIFT_APIKEY = "your Datasift API key here"
